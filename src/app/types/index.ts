a8f7e1a146522da83a8050876726603e99de6685
Pablo Calle <callejupa@gmail.com>
2026-02-02 12:56:43 -0500

    add categories functions and data mockup

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -15,6 +15,15 @@ export type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>
 
+export type Category = {
+    displayName: string
+    slug: string
+}
+
+export type CategoriesData = {
+    categories: Category[]
+}
+
 export type ModelDetailPageProps = {
     params: Promise<{
         id: string
@@ -22,6 +31,12 @@ export type ModelDetailPageProps = {
 }
 
 // Components Types
+
+export type ModelsGridProps = {
+    title: string
+    models: Model[]
+}
+
 export type ModelCardProps = {
     model: Model
 }
